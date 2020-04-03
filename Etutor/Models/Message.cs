b986cba52b47bfa13afec47830858afe6103eb08
unknown <phongttit99@gmail.com>
2020-04-03 16:18:25 +0700

    Login and Assign

---
+++ Etutor/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Etutor.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int StdId { get; set; }
+        public int TutId { get; set; }
+        public virtual Assign Assign { get; set; }
+    }
+}
