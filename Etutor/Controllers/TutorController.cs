ddc41f2432299f4b4f2b2faf72d04473444208af
unknown <phongttit99@gmail.com>
2020-04-25 13:47:14 +0700

    gitignore

--- Etutor/Controllers/TutorController.cs
+++ Etutor/Controllers/TutorController.cs
@@ -8,18 +8,24 @@ using System.Web.Mvc;
 
 namespace Etutor.Controllers
 {
+    [Authorize]
     public class TutorController : Controller
     {
         private EtutorContext db = new EtutorContext();
         // GET: Tutor
-        public ActionResult Index(int? id)
-        {
-            if (id != null)
-            {
-                Session["TU_ID"] = id;
-            }
-            else { id = Convert.ToInt32(Session["TU_ID"]); }
+        //public ActionResult Index(int? id)
+        //{
+        //    if (id != null)
+        //    {
+        //        Session["TU_ID"] = id;
+        //    }
+        //    else { id = Convert.ToInt32(Session["TU_ID"]); }
+        //    var found = db.Documents.Where(m => m.Assign.TutorId == id).ToList();
+        //    return View();
+        //}
 
+        public ActionResult Chat()
+        {
             return View();
         }
     }
